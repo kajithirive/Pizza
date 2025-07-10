4437c0d706a679eb118b676b387ec0f73a6d342d
Thiriveedhi <kaji.thiriveedhi@capgemini.com>
2025-07-10 18:51:30 +0530

    new card for Venkat

--- src/App.js
+++ src/App.js
@@ -1,91 +1,68 @@
-import React from "react";
-import photo from "./Img.png"
 import "./style.css"
-// import Header from "./Header";
-// import Pizza from "./Pizza.js";
-// const pizzaData = [
-//   {
-//     name: "Focaccia",
-//     ingredients: "Bread with italian olive oil and rosemary",
-//     price: 6,
-//     photoName: "pizzas/focaccia.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Margherita",
-//     ingredients: "Tomato and mozarella",
-//     price: 10,
-//     photoName: "pizzas/margherita.jpg",
-//     soldOut: true,
-//   },
-//   {
-//     name: "Pizza Spinaci",
-//     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-//     price: 12,
-//     photoName: "pizzas/spinaci.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Funghi",
-//     ingredients: "Tomato, mozarella, mushrooms, and onion",
-//     price: 12,
-//     photoName: "pizzas/funghi.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Salamino",
-//     ingredients: "Tomato, mozarella, and pepperoni",
-//     price: 15,
-//     photoName: "pizzas/salamino.jpg",
-//     soldOut: true,
-//   },
-//   {
-//     name: "Pizza Prosciutto",
-//     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-//     price: 18,
-//     photoName: "pizzas/prosciutto.jpg",
-//     soldOut: false,
-//   },
-// ];
- 
+import pic from "./Img.png"
 
-// export default function App () {
-//   return (
-//     <>
-//      <Header/>
-//     <Pizza props={pizzaData}/>
-
-//     </>
-//   )
+const skills = [
+    {
+      skill: "HTML+CSS",
+      level: "advanced",
+      color: "#2662EA"
+    },
+    {
+      skill: "JavaScript",
+      level: "advanced",
+      color: "#EFD81D"
+    },
+    {
+      skill: "Web Design",
+      level: "advanced",
+      color: "#C3DCAF"
+    },
+    {
+      skill: "Git and GitHub",
+      level: "intermediate",
+      color: "#E84F33"
+    },
+    {
+      skill: "React",
+      level: "advanced",
+      color: "#60DAFB"
+    },
+    {
+      skill: "Svelte",
+      level: "beginner",
+      color: "#FF3B00"
+    }
+  ];
    
-// }
-function Skill(props){
-    return(
-        <div className="skill" style={{ backgroundColor: props.colour}}>
-            <span>{props.course}</span>
-            <span> {props.emoji}</span>
-        </div>
-   
-    )
-}
-function SkillList(){
-    return(
-        <div className="skill-list">
-        <Skill course="React" emoji="thumb" colour="red"/>
-        <Skill course="CSS" emoji="hp" colour="blue"/>
-        <Skill course="Java" emoji="terra" colour="Yellow"/>
-        <Skill course="AWS" emoji="Azure" colour="green"/>
-        </div>
-    )
-}
-export default function App() {
+function Skill({skill, level, color}) {
     return (
-       <div className="card">
-        <div className="">
-        <img className="avatar"src={photo} alt="Description" />
-         <p>Here is the discription</p>
-         <SkillList />
-         </div>   
-       </div> 
+    <div className="skill" style={{ backgroundColor: color }} >
+        <span>{skill}</span>
+        <span>{level=="advanced" && "💪"}</span>
+        <span>{level=="beginner" && "👶"}</span>
+        <span>{level=="intermediate" && "👍"}</span>
+    </div>
     )
+}
+
+function SkillList() {
+    return(
+        <div className="skill-list" > {
+        skills.map((skill)=><Skill skill={skill.skill} level={skill.level} color={skill.color}/>)}
+        </div>
+    )
+
+}
+
+export default function App (){
+    return(
+        <div className="card">
+        <img className="avatar" src={pic}  />
+        <h2 className="body">Venkat</h2>
+        <p>Hi Venkat</p>
+        <SkillList />
+        </div>
+
+    )
+   
 }
